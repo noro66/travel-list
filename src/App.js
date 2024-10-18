90a2d012d86d99cf70ca5d1be58f47b985e9e8c0
Nouaamane <norou6@gmail.com>
2024-10-18 22:00:35 +0100

    add : rendring items list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "charger", quantity: 16, packed: true },
+];
+function Item({item}){
+return <li> 
+  <span style={item.packed ? {textDecoration : "line-through"} : {}}>{item.quantity} {item.description}</span>
+<button style={{color: "red", fontSize: "32px"}}>&times;</button>
+</li>
+}
+
+
 
 function Logo(){
   return <h1>Far Away </h1>
@@ -8,7 +21,11 @@ function Form(){
   </div>
 }
 function PackingList(){
-  return <div className="list">LIST</div>
+  return <div className="list">
+        <ul>
+         {initialItems.map(item => <Item item={item} />)}
+        </ul>
+  </div> 
 }
 function Stats(){
   return <footer className="stats">
