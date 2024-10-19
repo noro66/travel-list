4d51bed4a1c93856f83653acba9d66fcd554ca30
Nouaamane <norou6@gmail.com>
2024-10-19 21:41:47 +0100

    feat implementing sorting by status description and input

--- src/App.js
+++ src/App.js
@@ -6,25 +6,32 @@ const initialItems = [
   { id: 3, description: "charger", quantity: 16, packed: true },
 ];
 
-
 function App() {
   const [items, setItems] = useState(initialItems);
 
-  function handelItems(item){
+  function handelItems(item) {
     setItems((prevItems) => [...prevItems, item]);
-  } 
-  function handelDelete(id){
-    setItems((prevItems)=> prevItems.filter(item => item.id !== id))
   }
-  function handelToogle(id){
-    setItems(prevItems => prevItems.map(item => item.id === id ? {...item, packed : !item.packed } : item))
+  function handelDelete(id) {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+  function handelToogle(id) {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
   }
   return (
     <div className="app">
       <Logo />
       <Form handelAddItem={handelItems} />
-      <PackingList  items={items} hanDeleteItem={handelDelete} handelToogle={handelToogle}/>
-      <Stats />
+      <PackingList
+        items={items}
+        hanDeleteItem={handelDelete}
+        handelToogle={handelToogle}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -32,11 +39,20 @@ function App() {
 function Item({ item, handelDeleteItem, handelToogle }) {
   return (
     <li>
-      <input onChange={()=> handelToogle(item.id)} type="checkbox"  />
+      <input
+        checked={item.packed}
+        onChange={() => handelToogle(item.id)}
+        type="checkbox"
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button onClick={()=> handelDeleteItem(item.id)} style={{ color: "red", fontSize: "32px" }}>&times;</button>
+      <button
+        onClick={() => handelDeleteItem(item.id)}
+        style={{ color: "red", fontSize: "32px" }}
+      >
+        &times;
+      </button>
     </li>
   );
 }
@@ -45,10 +61,9 @@ function Logo() {
   return <h1>Far Away </h1>;
 }
 
-function Form({handelAddItem}) {
+function Form({ handelAddItem }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-
 
   function handelSubmit(e) {
     e.preventDefault();
@@ -60,7 +75,6 @@ function Form({handelAddItem}) {
 
     setDescription("");
     setQuantity(1);
-  
   }
 
   return (
@@ -87,24 +101,63 @@ function Form({handelAddItem}) {
   );
 }
 
-function PackingList({items, hanDeleteItem, handelToogle}) {
+function PackingList({ items, hanDeleteItem, handelToogle }) {
+  const [sortedBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortedBy === "input") sortedItems = items;
+  if (sortedBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortedBy === "status")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(b.packed) - Number(a.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
-          <Item item={item} key={item.id} handelDeleteItem={hanDeleteItem} handelToogle={handelToogle} />
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            handelDeleteItem={hanDeleteItem}
+            handelToogle={handelToogle}
+          />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortedBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="status">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list !</em>
+      </p>
+    );
+  }
+  const itemsNum = items.length;
+  const itemsPacked = items.filter((item) => item.packed).length;
+  const percenage = Math.round((itemsPacked / itemsNum) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X</em>
+      <em>
+        {percenage === 100
+          ? "You got everything! Ready to go"
+          : `You have ${itemsNum} items on your list, and you already packed ${itemsPacked} item (${percenage}%) `}
+      </em>
     </footer>
   );
 }
-
 
 export default App;
